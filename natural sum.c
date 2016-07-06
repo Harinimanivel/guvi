c29bcadebcf0e9befc62ef2eb68385298ff9cddd
Harinimanivel <harinimanivel22@gmail.com>
2016-07-06 16:30:39 +0530

    Create natural sum.c

---
+++ natural sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int ctr,num,sum=0;
+clrscr();
+printf("Enter the number");
+scanf("%d",&num);
+for(ctr=0;ctr<n;ctr++)
+sum=sum+ctr;
+printf("%d",sum);
+getch();
+return 0;
+}
